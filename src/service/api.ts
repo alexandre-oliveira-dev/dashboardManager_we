8d202c91004855e66c63f65ead31919d48613472
Alexandre_oliveiradev <alexandredev.frontend@gmail.com>
2024-05-18 11:38:09 -0300

    update

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 
 export const api =  axios.create({
-    baseURL:'http://52.67.38.246:5000/'
+    baseURL:'https://52.67.38.246:5000/'
     // baseURL:'http://localhost:5000'
 })
