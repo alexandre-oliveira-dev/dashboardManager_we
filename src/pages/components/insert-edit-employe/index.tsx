bd4904084e487e1bfb283774c4682090588ca9fe
Alexandre_oliveiradev <alexandredev.frontend@gmail.com>
2024-05-20 09:45:46 -0300

    update

--- src/pages/components/insert-edit-employe/index.tsx
+++ src/pages/components/insert-edit-employe/index.tsx
@@ -12,7 +12,7 @@ import {Employes, createEmploye, updateEmploye} from "@/service/routes";
 import {useEffect, useState} from "react";
 import dayjs from "dayjs";
 import {useRouter} from "next/navigation";
-import {useAuth} from "@/app/useAuth";
+import {useAuth} from "@/pages/useAuth";
 
 interface InsertProps {
   data?: Employes["data"] | undefined;
